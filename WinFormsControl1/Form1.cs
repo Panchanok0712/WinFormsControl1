9578f5ec2ded616b7daa7dc45d4b559a34f9ab3f
Panchanok0712 <65040249135@udru.ac.th>
2024-11-18 11:56:29 +0700

    Add project files.

---
+++ WinFormsControl1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsControl1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, User", "First Message");
+            //MessageBox.Show("Hello, User", "Secand Message",MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("ต้องการปิดจริงหรือไม่", "แน่ใจนะ", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Random rn = new Random();
+            int r = rn.Next(0, 255);
+            int g = rn.Next(0, 255);
+            int b = rn.Next(0, 255);
+
+            this.BackColor = Color.FromArgb(r, g, b);
+            this.Text = r.ToString() + "," + g.ToString() + "," + b.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightSlateGray;
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.LightBlue;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.LightCyan;
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            // button3.BackColor = Color.LightCyan;
+        }
+
+        private void button3_MouseLeave(object sender, EventArgs e)
+        {
+            // button3.BackColor = Color.LightBlue;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "มหาวิทยาลัยราชภัฏอุดรธานี")
+            {
+                label1.Text = "Udonthani Rajabhat University";
+
+            }
+            else
+            {
+                label1.Text = "มหาวิทยาลัยราชภัฏอุดรธานี";
+            }
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.LightBlue;
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.LightSkyBlue;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+            Random rn = new Random();
+            int r = rn.Next(0, 255);
+            int g = rn.Next(0, 255);
+            int b = rn.Next(0, 255);
+            label1.ForeColor = Color.FromArgb(r, g, b);
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+           // textBox1.Text = "";
+            //textBox1.Text = string.Empty;
+            textBox1.Text = null;
+        }
+    }
+}
